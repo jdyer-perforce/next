37f4904506fa8a246c7f94149ec91b28004c60b4
jdyer-perforce <jdyer@perforce.com>
2024-10-31 20:04:50 +0000

    Add files via upload

--- app/components/NavbarButton.js
+++ app/components/NavbarButton.js
@@ -2,26 +2,26 @@ import { Button } from '@mui/material'
 import { usePathname } from 'next/navigation'
 
 const style = {
-	margin: "4px",
-	"&:hover": {
-		borderBottom: "2px solid lightgreen"
-	}
+  margin: "4px",
+  "&:hover": {
+    borderBottom: "2px solid lightgreen"
+  }
 }
 
 const activeStyle = {
-	borderBottom: "2px solid darkgreen"
+  borderBottom: "2px solid darkgreen"
 }
 
 export default function NavbarButton({ children, href, align }) {
-	const pathname = usePathname()
-	const active = pathname == href
-	return (
-		<Button href={href} disableRipple color="inherit" sx={[
-			style,
-			active ? activeStyle : null,
-			align == "right" ? { marginLeft: "auto" } : null
-		]}>
-			{children}
-		</Button>
-	)
+  const pathname = usePathname()
+  const active = pathname == href
+  return (
+    <Button href={href} disableRipple color="inherit" sx={[
+      style,
+      active ? activeStyle : null,
+      align == "right" ? { marginLeft: "auto" } : null
+    ]}>
+      {children}
+    </Button>
+  )
 }
